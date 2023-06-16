48bdd9d4dc5c79834547e869647321b6cfdf93d1
Sarwar <2plus7sarwar@gmail.com>
2023-06-16 11:28:46 +0600

    implemented user varification and payment system

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-require('dotenv').config(); 
+require('dotenv').config();
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-app.get('/', async(req, res) => {
-    res.send('sports-camp server runing');
+app.get('/', async (req, res) => {
+  res.send('sports-camp server runing');
 })
 
 
@@ -32,87 +34,113 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middleware to verify token
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization) {
+    return res.status(401).send({error: 'Unauthorized access'})
+  }
+  const token = authorization.split(' ')[1];
+
+  // verify token
+  jwt.verify(token, process.env.SECRET_TOKEN, (error, decoded) =>{
+    if(error) {
+      return res.status(401).send({error: 'Unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
 
     const coursesCollection = client.db('sportsCamp').collection('courses');
     const usersCollection = client.db('sportsCamp').collection('users');
     const selectedCollection = client.db('sportsCamp').collection('selected');
 
 
+    // // send token
+    app.post('/jwt', async(req, res) => {
+      const body = req.body;
+      const token = jwt.sign(body, process.env.SECRET_TOKEN, { expiresIn: 60 * 60 })
+      res.send({token});
+    })
+
+
     // get all users
-    app.get('/users', async(req, res) => {
+    app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
 
 
     // find current user
-    app.get('/currentUser', async(req,  res) => {
-        const userEmail = req.query.email;
-        const query = {email: userEmail};
-        const result = await usersCollection.findOne(query);
-        res.send(result);
+    app.get('/currentUser', async (req, res) => {
+      const userEmail = req.query.email;
+      const query = { email: userEmail };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
     })
 
 
     // find all courses
-    app.get('/courses', async(req, res) => {
-        const result = await coursesCollection.find().toArray();
-        res.send(result);
+    app.get('/courses', async (req, res) => {
+      const result = await coursesCollection.find().toArray();
+      res.send(result);
     })
-    
-    
+
+
     // find all instructors
-    app.get('/instructors', async(req, res) => {
-        const query = {role: "instructor"}
-        const result = await usersCollection.find(query).toArray();
-        res.send(result);
+    app.get('/instructors', async (req, res) => {
+      const query = { role: "instructor" }
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
     })
 
 
     // find top courses based of number of students
-    app.get('/topCourses', async(req, res) => {
-        const result = await coursesCollection.find().sort({numberOfStudents: -1}).toArray();
-        const slicedResult = result.slice(0, 6);
-        res.send(slicedResult);
+    app.get('/topCourses', async (req, res) => {
+      const result = await coursesCollection.find().sort({ numberOfStudents: -1 }).toArray();
+      const slicedResult = result.slice(0, 6);
+      res.send(slicedResult);
     })
 
 
     // find top instructors based of number of students
-    app.get('/topInstructors', async(req, res) => {
-        const query = {role: "instructor"}
-        const result = await usersCollection.find(query).sort({numberOfStudents: -1}).toArray();
-        const slicedResult = result.slice(0, 6);
-        res.send(slicedResult);
+    app.get('/topInstructors', async (req, res) => {
+      const query = { role: "instructor" }
+      const result = await usersCollection.find(query).sort({ numberOfStudents: -1 }).toArray();
+      const slicedResult = result.slice(0, 6);
+      res.send(slicedResult);
     })
 
 
     // add an user
-    app.post('/users', async(req, res) => {
-        const userInfo = req.body;
-        // check if the user already exists while continue with google
-        const isExists = await usersCollection.findOne({email: userInfo.email});
-        if(isExists) {
-          return;
-        }
-        
-        const user = {
-            name: userInfo.name,
-            email: userInfo.email,
-            photo: userInfo.photo,
-            role: userInfo.role
-        }
+    app.post('/users', async (req, res) => {
+      const userInfo = req.body;
+      // check if the user already exists while continue with google
+      const isExists = await usersCollection.findOne({ email: userInfo.email });
+      if (isExists) {
+        return;
+      }
 
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
+      const user = {
+        name: userInfo.name,
+        email: userInfo.email,
+        photo: userInfo.photo,
+        role: userInfo.role
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
     })
 
 
     // add seleceted items
-    app.post('/selectedItems', async(req, res) => {
+    app.post('/selectedItems', async (req, res) => {
       const itemInfo = req.body;
       const result = await selectedCollection.insertOne(itemInfo);
       res.send(result);
@@ -120,16 +148,16 @@ async function run() {
 
 
     // get selected items for sepecific user
-    app.get('/selectedItems', async(req, res) => {
+    app.get('/selectedItems', verifyJWT, async (req, res) => {
       const userEmail = req.query.email;
-      const query = {userEmail: userEmail};
+      const query = { userEmail: userEmail };
       const result = await selectedCollection.find(query).toArray();
       res.send(result);
     })
 
 
     // add new course
-    app.post('/courses', async(req, res) => {
+    app.post('/courses', async (req, res) => {
       const data = req.body;
       const courseInfo = {
         courseName: data.courseName,
@@ -144,33 +172,33 @@ async function run() {
       }
       const result = await coursesCollection.insertOne(courseInfo);
       res.send(result);
-      
+
     })
 
 
     // get courses by specific instructor
-    app.get('/coursesByInstructor', async(req, res) => {
+    app.get('/coursesByInstructor', async (req, res) => {
       const instructorEmail = req.query.email;
-      const query = {instructorEmail: instructorEmail};
+      const query = { instructorEmail: instructorEmail };
       const result = await coursesCollection.find(query).toArray();
       res.send(result);
     })
 
 
     // delete course from selected
-    app.delete('/selectedCourses/:id', async(req, res) => {
+    app.delete('/selectedCourses/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await selectedCollection.deleteOne(query);
       res.send(result);
     })
 
 
     // update user role to instructor
-    app.patch('/makeInstructor/:id', async(req, res) => {
+    app.patch('/makeInstructor/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const options = {upsert: true};
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
       const doc = {
         $set: {
           role: 'instructor'
@@ -179,12 +207,12 @@ async function run() {
       const result = await usersCollection.updateOne(query, doc, options);
       res.send(result);
     })
-    
+
     // update user role to admin
-    app.patch('/makeAdmin/:id', async(req, res) => {
+    app.patch('/makeAdmin/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const options = {upsert: true};
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
       const doc = {
         $set: {
           role: 'admin'
@@ -193,13 +221,13 @@ async function run() {
       const result = await usersCollection.updateOne(query, doc, options);
       res.send(result);
     })
-    
+
 
     // update course status to approved
-    app.patch('/approve/:id', async(req, res) => {
+    app.patch('/approve/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
-      const options = {upsert: true};
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
       const doc = {
         $set: {
           status: 'approved',
@@ -209,15 +237,15 @@ async function run() {
       const result = await coursesCollection.updateOne(query, doc, options);
       res.send(result);
     })
-    
-    
+
+
     // update course status to denied
-    app.patch('/deny/:id', async(req, res) => {
+    app.patch('/deny/:id', async (req, res) => {
       const id = req.params.id;
       const data = req.body;
-      
-      const query = {_id: new ObjectId(id)};
-      const options = {upsert: true};
+
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
       const doc = {
         $set: {
           status: 'denied',
@@ -230,7 +258,20 @@ async function run() {
 
 
 
+    // payment intent
+    app.post('/create-payment-intent', async (req, res) => {
+      const { price } = req.body; 
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        automatic_payment_methods: ['card']
 
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
+    })
 
 
 
@@ -258,6 +299,6 @@ run().catch(console.dir);
 
 
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+  console.log(`server is running on port ${port}`);
 })
 
